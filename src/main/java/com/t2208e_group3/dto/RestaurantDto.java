11242be55ea60958554e95ac98d30ff1bdeb643f
giang8692@gmail.com <giang8692@gmail.com>
2024-07-30 22:45:17 +0700

    upload project

---
+++ src/main/java/com/t2208e_group3/dto/RestaurantDto.java
@@ -0,0 +1,20 @@
+package com.t2208e_group3.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
+public class RestaurantDto {
+    private Long id;
+    private String title;
+
+    @Column(length = 1000)
+    private List<String> images;
+
+    private String description;
+
+}
---
+++ src/main/java/com/t2208e_group3/model/USER_ROLE.java
@@ -0,0 +1,7 @@
+package com.t2208e_group3.model;
+
+public enum USER_ROLE {
+    ROLE_CUSTOMER,
+    ROLE_RESTAURANT_OWNER,
+    ROLE_ADMIN
+}
