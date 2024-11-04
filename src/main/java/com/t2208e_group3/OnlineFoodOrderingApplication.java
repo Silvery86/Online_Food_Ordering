3ef43ad36b6f7f9a4cdca64c97fd6eec89ea349c
Silvery86 <giang8692@gmail.com>
2024-11-04 10:18:08 +0700

    Update: Update Timezone for App

--- src/main/java/com/t2208e_group3/OnlineFoodOrderingApplication.java
+++ src/main/java/com/t2208e_group3/OnlineFoodOrderingApplication.java
@@ -3,10 +3,13 @@ package com.t2208e_group3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class OnlineFoodOrderingApplication {
-
 	public static void main(String[] args) {
+		// Set the application's default timezone
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(OnlineFoodOrderingApplication.class, args);
 	}
 
--- src/main/java/com/t2208e_group3/config/VnPayService.java
+++ src/main/java/com/t2208e_group3/config/VnPayService.java
@@ -42,6 +42,7 @@ public class VnPayService {
 
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
